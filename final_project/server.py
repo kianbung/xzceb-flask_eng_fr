06cabf3f40e12dfb3361bc76b2d4d19fd9d2a587
Yeo Kian Bung <kianbung@gmail.com>
2022-04-17 09:06:02 +0000

    final commit

--- final_project/server.py
+++ final_project/server.py
@@ -1,20 +1,19 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
-from machinetranslation import english_to_french, french_to_english
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    fr_translation = english_to_french(textToTranslate)
+    fr_translation = translator.english_to_french(textToTranslate)
     return fr_translation
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    en_translation = french_to_english(textToTranslate)
+    en_translation = translator.french_to_english(textToTranslate)
     return en_translation
 
 @app.route("/")
